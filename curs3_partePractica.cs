f3c073182611056f34e75eee6da32ffe6cd36cb8
tipeiusara <tipeiusara@gmail.com>
2025-11-26 22:11:36 +0200

    Add files via upload

---
+++ curs3_partePractica.cs
@@ -0,0 +1,37 @@
+﻿namespace GalacticQuest
+{
+    internal class Program
+    {
+        enum GameAction
+        {
+            Monsters = 1,
+            Exit = 0
+        }
+
+        static void Main(string[] args)
+        {
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                string input = Console.ReadLine();
+                int.TryParse(input, out int parsedInput); // int parsedInput = input(din string in int)
+
+                switch (parsedInput)
+                {
+                    case (int)GameAction.Monsters: // if parsedInput == 1
+                        Console.WriteLine("Hello monsters");
+                        break;
+                    case (int)GameAction.Exit: // if parsedInput == 0
+                        Console.WriteLine("You will exit");
+                        isRunning = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
